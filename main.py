31dcec818aa8fa7dba3f2f117f714d9a71b9e1cb
programista10kpln <programista10kpln@proton.me>
2022-06-01 16:41:08 +0200

    list expression vs map

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('hello world')
+liczby = [2, 10, 12, 114, 16, 7778, 98]
+
+wynik = map(lambda x: x * 2, liczby)
+print(list(wynik))
+
+liczby_razy_dwa = [i * 2 for i in liczby]
+print(liczby_razy_dwa)
+
+
