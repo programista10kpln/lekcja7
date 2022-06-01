1ffb1d828138be080faa60f170a7acfec2410629
programista10kpln <programista10kpln@proton.me>
2022-06-01 16:50:06 +0200

    list expression vs map vs filter

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-liczby = [2, 10, 12, 114, 16, 7778, 98]
+liczby = [2, 10, 12, 114, 16, 7778, 98, 5]
 
-wynik = map(lambda x: x * 2, liczby)
+wynik = map(lambda x: x * 2, liczby) #modyfikuje listę zgodnie z działaniem 1 arg czyli funkcji
 print(list(wynik))
 
 liczby_razy_dwa = [i * 2 for i in liczby]
 print(liczby_razy_dwa)
 
+wynik_filtr = filter(lambda x: x % 2 == 0, liczby) #obcina to co nie spełnia warunku
+print(list(wynik_filtr))
 
+liczby_podzielne_przez_dwa = [i for i in liczby if i % 2 == 0]
+print(liczby_podzielne_przez_dwa)
